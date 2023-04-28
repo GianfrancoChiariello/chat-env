a6ccb6aba96a3a1c6f0c9f337d0a9e337e665c03
Gianfranco Chiariello <gchiariello@pentass.com>
2023-04-28 12:21:44 -0300

    feat(Chat): :memo:

---
+++ src/pages/chat.js
@@ -0,0 +1,66 @@
+import { useEffect, useState } from 'react'
+  import * as signalR from '@microsoft/signalr'
+  
+  
+  export default function chat() {
+      
+    const [messages,setMessages] = useState([])
+  
+      useEffect(() => {
+  
+        const conexion = new signalR.HubConnectionBuilder()
+        .withUrl('url')
+        .build()
+  
+        conexion.on('histoy messages', (user,message) => {
+          setMessages([...message, {user, message}]);
+        })
+  
+        conexion.start()
+        .then(() => {
+            console.log("canal conectado")
+        })
+        .catch(() => {
+            console.log("error al conectar")
+        })
+  
+        return () => {
+          conexion.stop()
+        }
+      },[messages])
+  
+  
+      const handleMessage = async () => {
+        const user = prompt("Ingrese su usuario")
+        const msg = prompt("ingrese msg")
+
+  
+        if (msg.length > 0 && user.length > 0) {
+            await conexion.invoke('new message', user, msg)
+            setMessages([...messages,{user, msg}])
+        }
+      } 
+  
+      
+      
+    
+    
+    return (
+      <>
+        <button onClick={() => handleMessage()}>Conectar</button>
+
+        <div>
+          <h1>Messages</h1>
+          {
+          messages.map((message) => {
+            <>
+              <h3>{message.user};</h3>
+              <strong>{message.message}</strong>
+            </>
+          })
+        }
+        </div>
+      </>
+    )
+  }
+  
